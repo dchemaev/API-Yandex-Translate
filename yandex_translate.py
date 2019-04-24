2fe67a92fc04bae919ef433079f15a2bffdebd55
dchemaev <dchemaev@yandex.ru>
2019-04-24 15:38:18 +0400

    Дополненный шаблон

--- yandex_translate.py
+++ yandex_translate.py
@@ -30,20 +30,39 @@ def main():
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
     if req['session']['new']:
-        res['response'][
-            'text'] = 'Здарова, оболтус!' \
-                      ' Пиши сюда свой текст и я его переведу,' \
-                      ' а потом позвоню твоей маме и расскажу, что ты не учишь английский!!!'
+        res['response']['text'] = 'Привет! Назови своё имя!'
         sessionStorage[user_id] = {
-            'tft': None
+            'tft': None,
+            'first_name': None
         }
         return
 
     if sessionStorage[user_id]['first_name'] is None:
-        tft = get_text(req)
         if req['request']['original_utterance'].lower() == 'помощь':
             res['response']['text'] = 'Бог тебе в помощь'
             return
+        first_name = get_first_name(req)
+        if first_name is None:
+            res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
+            res['response']['buttons'] = [
+                {
+                    'title': 'Помощь',
+                    'hide': True
+                }
+            ]
+            return
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            # создаём пустой массив, в который будем записывать города, которые пользователь уже отгадал
+            res['response'][
+                'text'] = f'Приятно познакомиться, {first_name.title()}, шучу, мне плевать как тебя зовут.' \
+                f' Я буду называть тебя оболтусом'
+
+            res['response'][
+                'text'] = 'Здарова, оболтус!' \
+                          ' Пиши сюда свой текст и я его переведу,' \
+                          ' а потом позвоню твоей маме и расскажу, что ты не учишь английский!!!'
+        tft = get_text(req)
         if tft is None:
             res['response']['text'] = 'Не расслышала текст. Повтори, пожалуйста!'
             res['response']['buttons'] = [
@@ -52,13 +71,9 @@ def handle_dialog(res, req):
                     'hide': True
                 }
             ]
-        if len(tft) != 3:
-            res['response'][
-                'text'] = "Чтобы запрос прошел тебе всего лишь нужно сказать: 'Переведи слово" \
-                          " (ваше слово, которое нужно перевести)'"
-        else:
-            translate(tft[2])
-        return
+            return
+        sessionStorage[user_id]['tft'] = tft
+        res['response']['text'] = f'{translate(tft)}'
 
 
 def translate(text):
@@ -71,17 +86,27 @@ def translate(text):
         "format": "plain"
     }
 
-    response = request.post(url, params)
-
-    return eval(response.text["text"])
+    response = request.get(url, params)
+    logging.info(eval(response)['text'][0])
+    return eval(response)['text'][0]  # translated text
 
 
 def get_text(req):
     text = []
+    for word in req['request']['nlu']['tokens']:
+        text.append(word)
+    if len(text) == 3:
+        return text[2]
 
-    for entity in req['request']:
-        text.append(entity)
-    return text
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name', то возвращаем её значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
 
 
 if __name__ == '__main__':
